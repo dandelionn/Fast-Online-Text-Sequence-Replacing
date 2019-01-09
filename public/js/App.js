09c91661ce21f51aa975a9c21ae61afdffb739b0
Michea, Paul <paul.michea@in-tech.com>
2019-01-09 16:08:43 +0200

    added js folde and file in public

---
+++ public/js/App.js
@@ -0,0 +1,17 @@
+//import axios from 'axios';
+
+function download(){
+    /*console.log('here');
+    axios.get('./download')
+            .then(response => {
+                console.log("Downloaded.");
+            })
+            .catch(error => {
+                console.log("Something went wrong with the download");
+            });*/
+    window.open('./download');
+}
+
+function AddNewItem(){
+    let list = document.querySelector(".item-list");
+}
